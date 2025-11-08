e6802bfb9b82286996d1294c0262aaa8adcc81be
Rigoberto <r.castro23@info.uas.edu.mx>
2025-11-07 19:54:28 -0700

    guardar inicio

---
+++ frontend/inicio.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/inicio.css">
+    <title>Inicio</title>
+</head>
+<body>
+    <h1>Pagina de Inicio</h1>
+
+    <div class="cuadroCerrarSesion">
+        
+    </div>
+    
+</body>
+</html>
